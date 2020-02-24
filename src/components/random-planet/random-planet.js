092929a17fcbe7e4141f78346d5acb89aa765a2d
Said Babayev <babaiev.info@gmail.com>
2020-02-24 03:01:50 +0200

    Добавил компонент индикатор загрузки из сервиса loading.io

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
+import Spinner from "../spinner";
 
 
 export default class RandomPlanet extends Component {
@@ -32,6 +33,7 @@ export default class RandomPlanet extends Component {
     
         const { planet: {id, name, population, rotationPeriod, diameter} } = this.state
 
+        return <Spinner />
 
         return (
             <div className="random-planet jumbotron rounded">
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-double-ring-8yjhz9j21lb">
+            <div class="ldio-epqghvxc2al">
+                <div></div>
+                <div></div>
+                <div><div></div></div>
+                <div><div></div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
